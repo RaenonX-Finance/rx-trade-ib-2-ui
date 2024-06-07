1c448de467a8b432129ff06db826c4e6cbc21bc5
RaenonX <raenonx0710@gmail.com>
2024-06-06 23:40:25 -0500

    Updated option chain to include change %
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/ui/options/chain/row.tsx
+++ src/ui/options/chain/row.tsx
@@ -4,7 +4,8 @@ import {askClassName, bidClassName, markPxClassName} from '@/components/colors/c
 import {usePxSelector} from '@/state/px/selector';
 import {ContractId} from '@/types/data/px';
 import {getMidPx, getPxSpread} from '@/utils/calc/px';
-import {formatPercent} from '@/utils/string';
+import {getChange} from '@/utils/math';
+import {formatNumber, formatPercent} from '@/utils/string';
 
 
 type Props = {
@@ -19,15 +20,22 @@ export const OptionChainDataCells = ({contractId}: Props) => {
     getMidPx(px) ??
     (!!px?.Mark && px.Mark > 1E-3 ? px.Mark : undefined);
   const theta = px?.Theta ? Math.abs(px.Theta) : undefined;
+  const changeInfo = getChange({original: px?.Close, after: base});
 
   return (
     <>
-      <td>{px?.Last?.toFixed(2) ?? '-'}</td>
+      <td className="whitespace-nowrap">
+        {px?.Last?.toFixed(2) ?? `c ${px?.Close?.toFixed(2)}` ?? '-'}
+      </td>
       <td className={bidClassName}>{px?.Bid?.toFixed(2) ?? '-'}</td>
       <td className={askClassName}>{px?.Ask?.toFixed(2) ?? '-'}</td>
       <td className={markPxClassName}>{px?.Mark?.toFixed(2) ?? '-'}</td>
-      <td>Change</td>
-      <td>%</td>
+      <td className={changeInfo?.textClass}>
+        {formatNumber({num: changeInfo?.changeAmt, digits: 2, sign: true})}
+      </td>
+      <td className={changeInfo?.textClass}>
+        {formatNumber({num: changeInfo?.changePct, digits: 2, sign: true})}
+      </td>
       <td>{px?.Delta?.toFixed(4) ?? '-'}</td>
       <td>{px?.Theta?.toFixed(4) ?? '-'}</td>
       <td>
