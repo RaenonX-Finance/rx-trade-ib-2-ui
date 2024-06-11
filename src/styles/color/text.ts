c17ad3c5a5e5f38fcd1095465c6f432f8ae94971
RaenonX <raenonx0710@gmail.com>
2024-06-11 18:41:00 -0500

    Refactored text color styling
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

---
+++ src/styles/color/text.ts
@@ -0,0 +1,10 @@
+import {clsx} from 'clsx';
+
+
+export const getMarketColorClassOfText = (num: number) => {
+  if (num === 0) {
+    return clsx('text-slate-300');
+  }
+
+  return clsx(num > 0 ? 'text-market-up' : 'text-market-down');
+};
