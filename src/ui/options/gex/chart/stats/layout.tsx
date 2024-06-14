ecdef36ec74c131012139f17b2687d537f4609f9
RaenonX <raenonx0710@gmail.com>
2024-06-14 04:33:12 -0500

    Added current spot % diff in GEX stats
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/ui/options/gex/chart/stats/layout.tsx
+++ src/ui/options/gex/chart/stats/layout.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 
 import {Flex} from '@/components/layout/flex/common';
+import {getMarketColorClassOfText} from '@/styles/color/text';
 import {formatFloat} from '@/utils/format/number/regular';
+import {formatSignedNumber} from '@/utils/format/number/signed';
 import {Nullable} from '@/utils/type';
 
 
 type Props = {
   name: string,
   value: Nullable<number>,
+  spotPx: number,
 };
 
-export const OptionsGexStatsLayout = ({name, value}: Props) => {
+export const OptionsGexStatsLayout = ({name, value, spotPx}: Props) => {
+  const diffPercent = value ? value / spotPx - 1 : null;
+
   return (
     <Flex direction="row" className="items-end justify-center gap-1">
       <span className="text-xs leading-none">{name}</span>
-      <span>{formatFloat(value ?? 5000)}</span>
+      <span>{formatFloat(value)}</span>
+      {
+        diffPercent != null &&
+        <span className={getMarketColorClassOfText(diffPercent)}>
+          {formatSignedNumber({num: diffPercent, sign: true})}%
+        </span>
+      }
     </Flex>
   );
 };
--- src/ui/options/gex/chart/stats/main.tsx
+++ src/ui/options/gex/chart/stats/main.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 
 import {Flex} from '@/components/layout/flex/common';
+import {useOptionGexUnderlyingPxSelector} from '@/state/option/selector';
 import {OptionsGexStatsLayout} from '@/ui/options/gex/chart/stats/layout';
 import {useOptionsGexStats} from '@/ui/options/gex/stats/hook';
+import {getReferencePx} from '@/utils/calc/tick';
 
 
 export const OptionsGexStats = () => {
   const gexStats = useOptionsGexStats();
+  const underlyingPx = useOptionGexUnderlyingPxSelector();
+
+  const underlyingCurrentPx = getReferencePx(underlyingPx);
 
   return (
     <Flex center direction="row" className="gap-2 leading-none">
       <OptionsGexStatsLayout
         name="Gamma Field"
         value={gexStats?.gammaField}
+        spotPx={underlyingCurrentPx}
       />
       <OptionsGexStatsLayout
         name="Gamma Flip"
         value={gexStats?.gammaFlip}
+        spotPx={underlyingCurrentPx}
       />
     </Flex>
   );
