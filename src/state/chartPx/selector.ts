77819d4dd0aca2e0313f9b2a2782aa0b396028b8
RaenonX <raenonx0710@gmail.com>
2024-06-14 22:48:53 -0500

    Updated return typing of `useChartDataSelector()`
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/state/chartPx/selector.ts
+++ src/state/chartPx/selector.ts
@@ -2,11 +2,12 @@ import {useSelector} from 'react-redux';
 
 import {ReduxState} from '@/state/types';
 import {ChartData, ChartDataIdentifier} from '@/types/data/chart';
+import {Nullable} from '@/utils/type';
 
 
-export const useChartDataSelector = (identifier: ChartDataIdentifier | undefined): ChartData | undefined => (
+export const useChartDataSelector = (identifier: Nullable<ChartDataIdentifier>): Nullable<ChartData> => (
   useSelector(
-    ({chartPx}: ReduxState) => !!identifier ? chartPx.data[identifier] : undefined,
+    ({chartPx}: ReduxState) => !!identifier ? chartPx.data[identifier] : null,
     // What should trigger the update is whether the close / timestamp of the last bar changed
     (prev, curr) => {
       const prevLast = prev?.bars.at(-1);
