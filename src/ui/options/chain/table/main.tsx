a6e1345aa5dc6764bd1f8e27f906691d97566039
RaenonX <raenonx0710@gmail.com>
2024-06-17 04:39:52 -0500

    Added change in option chain
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/ui/options/chain/table/main.tsx
+++ src/ui/options/chain/table/main.tsx
@@ -7,9 +7,12 @@ import {Table} from '@/components/table/main';
 import {TableRow} from '@/components/table/row';
 import {useOptionChainContractsSelector, useOptionChainDefinitionSelector} from '@/state/option/selector';
 import {usePxSelector} from '@/state/px/selector';
+import {getMarketColorClassOfText} from '@/styles/color/text';
 import {OptionChainHeaderCells} from '@/ui/options/chain/table/headerCells';
 import {OptionChainDataCells} from '@/ui/options/chain/table/row';
 import {getClosestStrikeFromContract} from '@/ui/options/common/utils';
+import {getReferencePx} from '@/utils/calc/tick';
+import {formatFloat} from '@/utils/format/number/regular';
 
 
 export const OptionChainTable = () => {
@@ -26,19 +29,31 @@ export const OptionChainTable = () => {
       header={
         <>
           <OptionChainHeaderCells/>
-          <td className="text-center">Strike</td>
+          <td className="!pr-1 text-center">Strike</td>
+          <td className="!pl-1 text-center">%</td>
           <OptionChainHeaderCells/>
         </>
       }
-      body={contracts.map(({strike, call, put}) => (
-        <TableRow key={strike} className="text-right">
-          <OptionChainDataCells contractId={call}/>
-          <td className={clsx('text-center', strike === closestStrike ? 'bg-sky-600' : 'bg-slate-700')}>
-            {strike}
-          </td>
-          <OptionChainDataCells contractId={put}/>
-        </TableRow>
-      ))}
+      body={contracts.map(({strike, call, put}) => {
+        const spotStrikeDiffPercent = (strike / getReferencePx(spotPx) - 1) * 100;
+
+        return (
+          <TableRow key={strike} className="text-right">
+            <OptionChainDataCells contractId={call}/>
+            <td className={clsx('!pr-1 text-center', strike === closestStrike ? 'bg-sky-600' : 'bg-slate-700')}>
+              {strike}
+            </td>
+            <td className={clsx(
+              '!pl-1 text-[75%]',
+              strike === closestStrike ? 'bg-sky-800' : 'bg-slate-900',
+              getMarketColorClassOfText(spotStrikeDiffPercent),
+            )}>
+              {formatFloat(spotStrikeDiffPercent)}%
+            </td>
+            <OptionChainDataCells contractId={put}/>
+          </TableRow>
+        );
+      })}
     />
   );
 };
